4732b58f97ea4a3f7d25ac114152322de8d37879
KaleKemosabe <kale.kemosabe@gmail.com>
2021-01-01 15:37:06 +0200

    api and basic user list

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-export class Navbar extends Component {
-// default title and icon for the page -- if not added to App.js as props -- if values added to App.js, will override defaultProps
-    static defaultProps = {
-        title: 'Github finder',
-        icon: 'fab fa-github'
-    };
-
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired
-    };
-
-    render() {
-        return (
-            <div>
-                <h1>
-                    <i className={this.props.icon} /> {this.props.title} </h1>
-            </div>
-        )
-    }
+const Navbar = ({ icon, title }) => {
+    return (
+        <nav className="navbar bg-primary">
+            <h1>
+                <i className={icon} /> {title} </h1>
+        </nav>
+    )
 }
+
+// default title and icon for the page -- if not added to App.js as props -- if values added to App.js, will override defaultProps
+Navbar.defaultProps = {
+    title: 'Github finder',
+    icon: 'fab fa-github'
+};
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+};
 
 export default Navbar

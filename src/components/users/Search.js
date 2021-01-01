0493c3857a4d523409b073c9c9cef2b1b9efe476
KaleKemosabe <kale.kemosabe@gmail.com>
2021-01-01 19:26:03 +0200

    add alert on empty search

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,4 +1,4 @@
-import React, { Component, Profiler } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export class Search extends Component {
@@ -9,13 +9,18 @@ export class Search extends Component {
     static propTypes = {
         searchUsers: PropTypes.func.isRequired,
         clearUsers: PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired
+        showClear: PropTypes.bool.isRequired,
+        setAlert: PropTypes.func.isRequired
     };
 
     onSubmit = (e) => {
         e.preventDefault();
-        this.props.searchUsers(this.state.text);
-        this.setState({ text: '' });
+        if (this.state.text === '') {
+            this.props.setAlert('Please enter something on search', 'light');
+        } else {
+            this.props.searchUsers(this.state.text);
+            this.setState({ text: '' });
+        };
     };
 
     onChange = e => this.setState({ text: e.target.value });
